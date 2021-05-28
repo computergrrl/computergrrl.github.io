c60d145b0da902e15e8bb5dc71a1bdfd1d9549ac
computergrrl <anani.mowse@gmail.com>
2021-05-28 13:04:36 -0700

    More modal fun

--- js/groovy.js
+++ js/groovy.js
@@ -15,13 +15,15 @@ const portfolioData = [
     title: "Photo Gallery App",
     shortDescription: "This app uses CSS Grid for a responsive layout of any collection of images.",
     fullDescription: "This app uses CSS Grid for a responsive layout of any collection of images. Implementation of the Lightbox plugin is used so that viewer can click on any image and see the full screen image along with a descriptive caption",
+    demoLink: "https://computergrrl.github.io/FEWD-P5-Interactive-Photo-Gallery/",
   },
 
   {
     img: "images/Web_style_guide.png",
     title: "Sass Web Style Guide",
     shortDescription: "A Sass based web style guide for web designers.",
-    fullDescription: "A Sass based web style guide for web designers. This web style guide makes it easy for any web developer to implement the outlined style guides into their own website. Includes styling for links, buttons, typography, blockquotes and grid structure."
+    fullDescription: "A Sass based web style guide for web designers. This web style guide makes it easy for any web developer to implement the outlined style guides into their own website. Includes styling for links, buttons, typography, blockquotes and grid structure.",
+    demoLink: "https://computergrrl.github.io/Web-Style-Guide-FEWD-P4/",
   },
 
   {
@@ -29,6 +31,7 @@ const portfolioData = [
     title: "Mystery Phrase Game V1",
     shortDescription: "This app is version 1 of a mystery phrase guessing game",
     fullDescription: "This app is a mystery phrase guessing game in which the user tries to guess the words in a galaxy space edition of hangman. User can choose to use either the on screen keyboard or their computer keyboard to guess letters before they run out of turns and the game is over. ",
+    demoLink: "https://computergrrl.github.io/FSJS-Project-4/",
   }, 
 
   {
@@ -36,6 +39,7 @@ const portfolioData = [
     title: "Name That Movie App",
     shortDescription: "This app is version 2 of the  mystery phrase guessing game.",
     fullDescription: "This app is version 2 of the  mystery phrase guessing game. In this version the user is given movie sound byte clues and must try to guess the name of the movie  before they run out of turns and the game is over. User can choose to use either the on screen keyboard or their computer keyboard to guess letters",
+    demoLink: "https://computergrrl.github.io/FEWD-P6-Game-Show-App/",
   },
 
   {
@@ -43,6 +47,7 @@ const portfolioData = [
     title: "Web Dashboard App",
     shortDescription: "This app is a dashboard which hosts a range features for user convenience.",
     fullDescription: "This app is a dashboard which hosts a range features for user convenience. Dashboard includes tools that can be used to monitor site activity, personalize user experience. Includes charts to view web traffic, a messaging app, and also a personal settings feature for users to set their timezones and preferences. ",
+    demoLink: "https://computergrrl.github.io/FEWD-P7-Web-App-Dashboard/",
   }, 
 
   {
@@ -50,6 +55,7 @@ const portfolioData = [
     title: "Employee Directory App",
     shortDescription: "This app connects with an API to create an employee directory.",
     fullDescription: "This app connects with an API to create an employee directory. The app pulls random users from an API and displays them on the page. User can click on any employee card to view a modal popup that includes further details and contact info about the selected employee, including address and DOB.",
+    demoLink: "https://computergrrl.github.io/FEWD-8-API-Employee-Directory/",
   }
 
 ];
@@ -87,12 +93,13 @@ cards.forEach((card) => {
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-          <img src="${portfolioData[index].img}">
           ${portfolioData[index].fullDescription}
+          <img src="${portfolioData[index].img}" pt-3  border border-dark>
           </div>
-          <div class="modal-footer">
+          <div class="modal-footer mx-auto">
+            <a class="btn btn-primary" href="${portfolioData[index].demoLink}" target="_blank">Live Demo</a>
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            
           </div>`;
     setInnerHtml(html);
     myModal.show();
